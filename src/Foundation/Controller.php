cb7208a2bd0a7a0b005bd083919a75cc78347326
MrAnyx <1ptitigre@gmail.com>
2020-09-24 18:50:48 +0200

    [fix] dependencies & controller/router files

--- src/Foundation/Controller.php
+++ src/Foundation/Controller.php
@@ -1,28 +1,23 @@
 <?php
 
 /**
- * La classe Controller permet de mettre en forme les futurs controllers
- * Elle embarque les attributs liés à la gestions de la base de données ainsi
- * qu'à l'envoie de la vue à l'utilisateur.
+ * PHP version 7.4.9
  * 
- * PHP version 7.4.1
- * 
- * @category Controller
- * @package TimePHP
- * @subpackage Foundation
  * @author Robin Bidanchon <robin.bidanchon@gmail.com>
- * @license MPL-2.0 https://github.com/MrAnyx/Skeleton-TimePHP/blob/master/LICENSE
- * @link any page
  */
-
 
 namespace TimePHP\Foundation;
 
-use PDO;
 use Twig\Environment;
 use Twig\TwigFunction;
-use Twig\Loader\FilesystemLoader;
 use TimePHP\Foundation\Router;
+use Twig\Loader\FilesystemLoader;
+use Illuminate\Container\Container;
+use Symfony\Component\Dotenv\Dotenv;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+
 
 /**
  * @category Controller
@@ -33,41 +28,55 @@ abstract class Controller
 {
 
     /**
-     * @var Environment
+     * @var Environment Permet de retourner la vue correspondante à l'utilisateur
      */
-    public $twig;
-
-
-    /**
-     * @var PDO
-     */
-    public $client;
+    protected $twig;
 
     public function __construct()
     {
         //chargement du fichier app.ini à la racine du projet
-        $lst_ini = parse_ini_file("../../app.ini");
-        $this->twig = new Environment(new FilesystemLoader(__DIR__ . "/../../". $lst_ini["view_path"]));
+        $dotenv = new Dotenv();
+        $dotenv->load(__DIR__ . '/../../config/.env');
+
+        $this->twig = new Environment(new FilesystemLoader(__DIR__ . "/../../". $_ENV['VIEW_PATH']));
 
         // ajout de la fonction asset pour twig afin de récuperer l'url du dossier asset dans le repertoire public
-        $this->twig->addFunction(new TwigFunction('asset', function ($asset)
+        $this->twig->addFunction(new TwigFunction('asset', function ($asset): string
         {
-            return sprintf('/../assets/%s', ltrim($asset, '/'));
+            return sprintf('/assets/%s', ltrim($asset, '/'));
         }));
-        $this->twig->addFunction(new TwigFunction('generate', function (string $name, array $params)
+        $this->twig->addFunction(new TwigFunction('component', function ($component): string
         {
-            return sprintf(Router::$router->generate($name, $params));
+            return sprintf('components/%s', ltrim($component, '/'));
         }));
-        $this->twig->addFunction(new TwigFunction('dump', function ($object)
+        $this->twig->addFunction(new TwigFunction('generate', function (string $nameUrl, array $params = [], array $flags = []): string
         {
-            return var_dump($object);
+            return sprintf(Router::generate($nameUrl, $params, $flags));
         }));
 
-        $str_connexion = "mysql:host=".$lst_ini["my_host"].";dbname=".$lst_ini["my_name"];
-        $this->client = new PDO($str_connexion, $lst_ini["my_user"], $lst_ini["my_pass"]);
-        // permet d'afficher un rapport des erreurs
-        $this->client->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        // indique que le mode par défaut pour le fetch est FETCH_ASSOC
-        $this->client->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->twig->addFunction(new TwigFunction('dump', function ($object): string
+        {
+            ob_start();
+            dump($object);
+            return ob_get_clean();
+        }));
+
+        $capsule = new Capsule;
+        $capsule->addConnection([
+            'driver'    => 'mysql',
+            'host'      => $_ENV['DB_HOST'],
+            'database'  => $_ENV['DB_NAME'],
+            'username'  => $_ENV['DB_USER'],
+            'password'  => $_ENV['DB_PASS'],
+            'port'      => $_ENV['DB_PORT'],
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
+        ]);
+        $capsule->setEventDispatcher(new Dispatcher(new Container));
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+
     }
+
 }
--- src/Foundation/Router.php
+++ src/Foundation/Router.php
@@ -1,18 +1,9 @@
 <?php
 
 /**
- * La classe Router sert à gérer les url et
- * associe les url au bon controller.
- * Le router sert également à afficher une belle page d'erreur.
+ * PHP version 7.4.9
  * 
- * PHP version 7.4.1
- * 
- * @category Router
- * @package TimePHP
- * @subpackage Foundation
  * @author Robin Bidanchon <robin.bidanchon@gmail.com>
- * @license MPL-2.0 https://github.com/MrAnyx/Skeleton-TimePHP/blob/master/LICENSE
- * @link any page
  */
 
 namespace TimePHP\Foundation;
@@ -35,20 +26,14 @@ class Router
     public static $router;
 
     /**
-     * @var int Variable qui permet de calculer le temps de chargement
-     */
-    public static $loadingTime;
-
-    /**
-     * @var Whoops Générateur de belles erreurs
+     * @var Whoops Générateur de belles pages d'erreurs
      */
     private $_whoops;
 
-    //Class constructor
     public function __construct()
     {
         self::$router = new AltoRouter();
-        self::$loadingTime = microtime(true);
+        self::$router->addMatchTypes(array('s' => '[a-z0-9]+(?:-[a-z0-9]+)*')); // slug
         $this->_whoops = new Run;
         $this->_whoops->pushHandler(new PrettyPageHandler);
         $this->_whoops->register();
@@ -68,43 +53,68 @@ class Router
         return $this;
     }
 
+
+    /**
+     * Permet d'ajouter une nouvelle route avec la méthode post
+     * 
+     * @param string $url Url utilisée pour lancer le controller
+     * @param object $object Fonction or String representant le controller
+     * @param string|null $name (optional) name of the path
+     * @return self Permet de faire du fluant calling
+     */
+    public function post(string $url, $object, ?string $name): self
+    {
+        self::$router->map("POST", $url, $object, $name);
+        return $this;
+    }
+
     /**
      * Permet de generer une url via le nom de la route
      * 
      * @param string $name Correspond au nom de la route que l'on souhaite
      * @param array|null $params (optionel) correspond au parametres à donner a l'url
      * @return string
-     * @deprecated 
      */
-    public function url(string $name, array $params): string
+    public static function generate(string $name, array $params = [], array $flags = []): string
     {
-        return self::$router->generate($name, $params);
+        $url = self::$router->generate($name, $params);
+        if(count($flags) === 0){
+            return $url;
+        } else {
+            $index = 0;
+            foreach($flags as $key => $value){
+                $index === 0 ? $url.="?" : $url.="&";
+                $url.=$key."=".$value;
+                $index++;
+            }
+            return $url;
+        }
     }
 
     /**
-     * Permet d'associer le bon controller / fonction avec l'url saisie
+     * Permet d'associer le bon controller / fonction avec l'url saisi
      */
     public function run()
     {
-        $lst_match = self::$router->match();
+        $match = self::$router->match();
 
         // si l'url ne correspond à aucune des routes
-        if ($lst_match === false) {
+        if ($match === false) {
             header("Location: ".self::$router->generate("home")); //redirection vers la page d'accueil
 
-        // si on renseigne un controller (HomeController#function 
-        } else if(is_string($lst_match["target"])) {
-            list($controller, $action) = explode('#', $lst_match['target']);
+        // si on renseigne un controller (BlogController#function) 
+        } else if(is_string($match["target"])) {
+            list($controller, $function) = explode('#', $match['target']);
             $ctrl = "App\\Bundle\\Controllers\\".$controller;
-            if (is_callable(array(new $ctrl, $action))) {
-                call_user_func_array(array(new $ctrl,$action), $lst_match['params']);
+            if (is_callable(array(new $ctrl, $function))) {
+                call_user_func_array(array(new $ctrl,$function), $match['params']);
             } else {
                 header('HTTP/1.1 500 Internal Server Error');
             }
         
         // si on renseigne une fonction au lieu d'un controller
-        } else if(is_object($lst_match["target"]) && is_callable($lst_match["target"])) {
-            call_user_func_array($lst_match["target"], $lst_match["params"]);
+        } else if(is_object($match["target"]) && is_callable($match["target"])) {
+            call_user_func_array($match["target"], $match["params"]);
         } else {
             header('HTTP/1.1 500 Internal Server Error');
         }
